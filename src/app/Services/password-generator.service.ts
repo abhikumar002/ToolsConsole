4e1be34e34e6a8612562d8a7a53f5507d3a80fad
Abhishek kumar Gupta <abhishekkumar.gupta@42gears.com>
2024-08-14 01:11:36 +0530

    password-generator-Improve

--- src/app/Services/password-generator.service.ts
+++ src/app/Services/password-generator.service.ts
@@ -14,6 +14,12 @@ export class PasswordGeneratorService {
   public generatePassword(validChars: string, passwordLength: number): string{
 
     this.generatedPassword = '';
+    if(passwordLength > 3000)
+    {
+      //this.generatedPassword = "Password length is too long.";
+      alert("Password length is too long.");
+      return this.generatedPassword;
+    }
     for (let i = 0; i < passwordLength; i++) {
       const index = Math.floor(Math.random() * validChars.length);
       this.generatedPassword += validChars[index];
